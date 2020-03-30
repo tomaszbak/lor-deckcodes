c39000dbfd285aa7d7e8d5c09fa320ac5f319771
Tomasz Bąk <me@tomaszbak.engineer>
2020-03-30 20:38:41 +0200

    Use strategy pattern in DeckDecoder

--- Sources/LoRDeckCodes/DeckDecoder.swift
+++ Sources/LoRDeckCodes/DeckDecoder.swift
@@ -6,85 +6,91 @@ struct DeckDecoder {
             throw DecodingError.emptyInput
         }
         
-        let context = DecodingContext(step: .header)
-        let result = try input.reduce(context, decode)
-        return Deck(cards: result.cards)
-    }
-    
-    private func decode(context: DecodingContext, value: Int) throws -> DecodingContext {
-        switch context.step {
-        case .header:
-            return try parseHeader(value, with: context)
-        case .numberOfSections:
-            return try parseNumberOfSections(value, with: context)
-        case .numberOfCardsInSection:
-            return try parseNumberOfCardsInSection(value, with: context)
-        case .set:
-            return try parseSet(value, with: context)
-        case .faction:
-            return try parseFaction(value, with: context)
-        case .card:
-            return try parseCard(value, with: context)
+        let result = try input.reduce(HeaderStep()) { step, value -> Step in
+            var step = step
+            return try step.evaluate(value)
         }
+        return Deck(cards: result.context.cards)
     }
+}
+
+private struct DecodingContext {
+    var numberOfCards = 3
+    var sectionsLeft = 0
+    var cardsLeft = 0
+    var set: Set?
+    var faction: Faction?
     
-    private func parseHeader(_ value: Int, with context: DecodingContext) throws -> DecodingContext {
+    var cards: [Card] = []
+}
+
+private protocol Step {
+    var context: DecodingContext { get }
+    mutating func evaluate(_ value: Int) throws -> Step
+}
+
+private struct HeaderStep: Step {
+    var context = DecodingContext()
+    
+    func evaluate(_ value: Int) throws -> Step {
         let format = (value & 0b11110000) >> 4
         let version = value & 0b00001111
         
         guard format == 1, version == 1 else {
             throw DecodingError.invalidFormat(Int(format), Int(version))
         }
-        
-        var context = context
-        context.step = .numberOfSections
-        return context
+
+        return NumberOfSectionsStep(context: context)
     }
     
-    private func parseNumberOfSections(_ value: Int, with context: DecodingContext) throws -> DecodingContext {
-        var context = context
-        
+}
+
+private struct NumberOfSectionsStep: Step {
+    var context: DecodingContext
+    
+    mutating func evaluate(_ value: Int) throws -> Step {
         context.sectionsLeft = value
         
         if context.sectionsLeft > 0 {
-            context.step = .numberOfCardsInSection
+            return NumberOfCardsInSectionStep(context: context)
         } else {
             context.numberOfCards -= 1
-            context.step = .numberOfSections
+            return NumberOfSectionsStep(context: context)
         }
-        
-        return context
     }
+}
+
+private struct NumberOfCardsInSectionStep: Step {
+    var context: DecodingContext
     
-    private func parseNumberOfCardsInSection(_ value: Int, with context: DecodingContext) throws -> DecodingContext {
-        var context = context
-        
+    mutating func evaluate(_ value: Int) throws -> Step {
         context.cardsLeft = value
-        context.step = .set
-        
-        return context
+        return SetStep(context: context)
     }
+}
+
+private struct SetStep: Step {
+    var context: DecodingContext
     
-    private func parseSet(_ value: Int, with context: DecodingContext) throws -> DecodingContext {
-        var context = context
-        
+    mutating func evaluate(_ value: Int) throws -> Step {
         context.set = Set(rawValue: value)
-        context.step = .faction
-        
-        return context
+        return FactionStep(context: context)
     }
+}
+
+private struct FactionStep: Step {
+    var context: DecodingContext
     
-    private func parseFaction(_ value: Int, with context: DecodingContext) throws -> DecodingContext {
-        var context = context
-        
+    mutating func evaluate(_ value: Int) throws -> Step {
         context.faction = Faction(rawValue: value)
-        context.step = .card
-        
-        return context
+        return CardStep(context: context)
     }
+}
+
+private struct CardStep: Step {
+    var context: DecodingContext
     
-    private func parseCard(_ value: Int, with context: DecodingContext) throws -> DecodingContext {
-        var context = context
+    mutating func evaluate(_ value: Int) throws -> Step {
         guard let faction = context.faction else {
             throw DecodingError.invalidFaction
         }
@@ -95,35 +101,19 @@ struct DeckDecoder {
         
         let card = Card(set: set, faction: faction, identifier: value, numberOfCopies: context.numberOfCards)
         context.cards.append(card)
-        
         context.cardsLeft -= 1
         
-        if context.cardsLeft == 0 {
-            context.sectionsLeft -= 1
-            
-            if context.sectionsLeft == 0 {
-                context.numberOfCards -= 1
-                context.step = .numberOfSections
-            } else {
-                context.step = .numberOfCardsInSection
-            }
+        guard context.cardsLeft == 0 else {
+            return CardStep(context: context)
         }
         
-        return context
+        context.sectionsLeft -= 1
+        
+        guard context.sectionsLeft == 0 else {
+            return NumberOfCardsInSectionStep(context: context)
+        }
+        
+        context.numberOfCards -= 1
+        return NumberOfSectionsStep(context: context)
     }
-}
-
-private enum Step {
-    case header, numberOfSections, numberOfCardsInSection, set, faction, card
-}
-
-private struct DecodingContext {
-    var step: Step
-    var numberOfCards = 3
-    var sectionsLeft = 0
-    var cardsLeft = 0
-    var set: Set?
-    var faction: Faction?
-    
-    var cards: [Card] = []
 }
